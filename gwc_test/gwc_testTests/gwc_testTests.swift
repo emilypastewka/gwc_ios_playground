8c9919bfff902734cc9788f60f02ee5882d5dc30
Emily Bailey <emilyb@uber.com>
2017-04-13 15:43:14 -0700

    work done 170412 by OCHS students (brand new to xcode, woohoo)

---
+++ gwc_test/gwc_testTests/gwc_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  gwc_testTests.swift
+//  gwc_testTests
+//
+//  Created by Emily Bailey on 4/13/17.
+//  Copyright © 2017 Emily Bailey. All rights reserved.
+//
+
+import XCTest
+@testable import gwc_test
+
+class gwc_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
