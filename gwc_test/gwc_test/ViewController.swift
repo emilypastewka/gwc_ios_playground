ffbdcdf7199766e8d213904d3092dd30f945f6ba
Emily Bailey <emilyb@uber.com>
2017-04-20 01:18:33 -0700

    girls learned to connect ui to code

--- gwc_test/gwc_test/ViewController.swift
+++ gwc_test/gwc_test/ViewController.swift
@@ -8,17 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    //MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    var newText = String()
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        newText = textField.text!
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: Actions
+    @IBAction func labelButton(_ sender: UIButton) {
+        //nameLabel.text = nameTextField.text
+        nameLabel.text = newText
+    }
+			
 
 
 }
